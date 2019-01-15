81c71efda15c082a48754f6bd7daceec60e73786
zhuxiaosong <1620165757@qq.com>
2019-01-16 00:01:03 +0800

    new原理学习

--- test.js
+++ test.js
@@ -1,20 +1,5 @@
 (() => {
-    // let z = Symbol();
-    // let a = {
-    //     x: Symbol(),
-    //     z:1
-    // };
-    let a = Symbol('a');
-    let b = Symbol('b');
-    let c = Symbol('c');
-    const obj = {
-        [c]: 1
-    };
 
 
-    obj[a] = 'Hello';
-    obj[b] = 'World';
-    // for (let y in obj) {
-    //     console.log(y)
-    // }
+
 })();
